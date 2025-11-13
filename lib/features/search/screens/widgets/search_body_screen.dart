87c66f2acd4474ae19015659007cca36cb7c33c1
Abdelrahman Nasser <abdelrahmanhydara@gmail.com>
2025-11-13 17:09:31 +0200

    - added search screen

---
+++ lib/features/search/screens/widgets/search_body_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SearchBodyScreen extends StatelessWidget {
+  const SearchBodyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Text('Search Screen'),
+      ],
+    );
+  }
+}
